45061cf90f5a57ddfc756c5b14cde6ca07266c5d
Joshua Robinson <joshua.robinson@gmail.com>
2013-08-25 21:43:30 -0700

    Multi-threaded support

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all:		test_prime_pairs is-prime_test
 
 is-prime_test: is-prime_test.cc is-prime.cc is-prime.h
-	g++ -O2 -o is-prime_test is-prime_test.cc is-prime.cc
+	g++ -Wall -g -O2 -o is-prime_test is-prime_test.cc is-prime.cc
 
 test_prime_pairs:	test_prime_pairs.cc is-prime.h is-prime.cc
-		g++ -O2 -o test_prime_pairs test_prime_pairs.cc is-prime.cc
+		g++ -Wall -g -O2 -lpthread -o test_prime_pairs test_prime_pairs.cc is-prime.cc
--- test_prime_pairs.cc
+++ test_prime_pairs.cc
@@ -1,16 +1,40 @@
+#include <assert.h>
 #include <climits>
 #include <stdio.h>
+#include <pthread.h>
+#include <vector>
 
 #include "is-prime.h"
 
-int main(int argc, char* argv[]) {
-	printf("Hello, world!\n");
+using std::vector;
 
-	for (int i = 3; i < INT_MAX; i += 2) {
-		if (is_prime(i) && is_prime(i + 2)) {
-			printf("%d and %d\n", i, i + 2);
-		}
+const int kNumThreads = 8;
+
+namespace twin_primes {
+
+void TestAndMaybePrint(int n) {
+	if (is_prime(n) && is_prime(n + 2)) {
+		printf("%d and %d\n", n, n + 2);
+	}
+}
+
+void* FindTwinPrimes(void* thread_id) {
+	const int start_offset = 3 + 2 * (long)thread_id;
+	const int delta = 2 * kNumThreads;
+	for (int i = start_offset; i < INT_MAX; i += delta) {
+		TestAndMaybePrint(i);
+	}
+	pthread_exit(NULL);
+}
+
+}  // namespace twin_primes
+
+int main(int argc, char* argv[]) {
+	vector<pthread_t> threads(kNumThreads);
+	
+	for (int i = 0; i < kNumThreads; i++) {
+		pthread_create(&threads[i], NULL, twin_primes::FindTwinPrimes, (void *)i);
 	}
 
-	return 0;
+	pthread_exit(NULL);
 }
